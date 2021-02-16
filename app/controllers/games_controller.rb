e4fa035f691333c9c59f673d2c7f7abcc9d04ea3
roy yoo <royyoo123@gmail.com>
2021-02-16 09:37:18 -0800

    a

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,11 +3,12 @@ require 'json'
 
 class GamesController < ApplicationController
 	def new
-		@letters = Array.new(10) { ('A'..'Z').to_a.sample }
-		@row = ""
-		@letters.each do |letter|
- 			@row += " " + letter
- 		end
+		@row = Array.new(10) { ('A'..'Z').to_a.sample }.join(" ")
+		# @row = ""
+		# @letters.each do |letter|
+ 	# 		@row += " " + letter
+
+ 		
  	end
 		# def english_word?(word)
 		#   response = open("https://wagon-dictionary.herokuapp.com/#{word}")
@@ -19,13 +20,12 @@ class GamesController < ApplicationController
 		#   word.chars.all? { |letter| word.count(letter) <= grid.count(letter) }
 		# end
 	def score
-
-
 		@row = params[:row]
 		@answer = params[:answer]
 		if in_grid?(@answer.upcase, @row.upcase)
 			if english_word?(@answer)
-				@result = "#{@answer} is in grid."
+				@result = "Congratulations! #{@answer} is in grid."
+
 				@score = get_score(@answer)
 			else
 				@result = "#{@answer} is not an English word"
@@ -33,23 +33,23 @@ class GamesController < ApplicationController
 		else
 			@result = "#{@answer} is not in the grid."
 		end
-
+		raise
 	end
-	
+
 	private
 
 	def english_word?(word)
-		  response = open("https://wagon-dictionary.herokuapp.com/#{word}")
-		  json = JSON.parse(response.read)
-		  return json['found']
-		end
+		response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+		json = JSON.parse(response.read)
+		return json['found']
+	end
 
-		def get_score(word)
-			word.length * 3
-		end
+	# def get_score(word)
+	# 	word.length ** 3 - word.length * 3
+	# end
 
-		def in_grid?(word, grid)
-		  word.chars.all? { |letter| word.count(letter) <= grid.count(letter) }
-		end
+	def in_grid?(word, grid)
+	  word.chars.all? { |letter| word.count(letter) <= grid.count(letter) }
+	end
 	
 end
